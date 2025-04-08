e69cc2d6e925bfa2f78454e97fc3c1906b031e91
Bruce Alves <brucealves800@gmail.com>
2025-04-08 00:04:39 -0300

    praticando sobre funções

---
+++ praticando/function/function1.js
@@ -0,0 +1,10 @@
+function number(){
+    let num = 3
+    if(num%2==0){
+        console.log('Número par')
+    }else{
+        console.log('Número impar')
+    }
+}
+
+number()
---
+++ praticando/function/function2.js
@@ -0,0 +1,8 @@
+function soma(a, b){
+    let a=3
+    let b=9
+    return a+b
+
+}
+
+soma()
